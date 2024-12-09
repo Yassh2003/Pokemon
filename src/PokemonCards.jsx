a44784e29584c4e084e04dd6230a40eda4c1867c
Yassh2003 <yashpratapindia789@gmail.com>
2024-12-10 00:07:46 +0530

    Added Name of Pokemon

--- src/PokemonCards.jsx
+++ src/PokemonCards.jsx
@@ -6,6 +6,7 @@ function PokemonCards({pokemonData}) {
         <figure>
             <img src={pokemonData.sprites.other.dream_world.front_default} alt={pokemonData.name} className='pokemon-image' />
         </figure>
+        <h1 className='pokemon-name'>{pokemonData.name}</h1>
     </li>
   )
 }
